b8a02708fd0ec89f1725890a277b6cfafcead8ae
Maythee Anegboonlap <contact@llun.dev>
2023-11-21 08:23:41 +0100

    Add Like schema

---
+++ src/like.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import { Note } from "./note.js";
+
+export const Like = z.object({
+  type: z.literal("Like"),
+  id: z.string(),
+  actor: z.string(),
+  object: z.union([z.string(), Note]),
+});
+
+export type Like = z.infer<typeof Like>;
