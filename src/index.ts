e5dcc83535e887824cfb992d81e29d715e2b54fb
Maythee Anegboonlap <contact@llun.dev>
2024-09-03 21:19:12 +0200

    Export Announce object

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from "./like.js";
 export * from "./note.js";
 export * from "./reject.js";
 export * from "./undo.js";
+export * from "./announce.js";
 
 export * as Mastodon from "./mastodon/index.js";
