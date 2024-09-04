232a42f2213e5db04aaa22d73358641d0aec6982
Maythee Anegboonlap <contact@llun.dev>
2024-09-04 21:00:10 +0200

    Add HashTag

--- src/note/tag.ts
+++ src/note/tag.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { Emoji } from "./emoji.js";
 import { Mention } from "./mention.js";
+import { HashTag } from "./hashtag.js";
 
-export const Tag = z.union([Mention, Emoji]);
+export const Tag = z.union([Mention, Emoji, HashTag]);
 export type Tag = z.infer<typeof Tag>;
