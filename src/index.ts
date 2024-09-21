2f197a83ad2df4e156abe642fac848ee06a71e04
Maythee Anegboonlap <contact@llun.dev>
2024-09-21 19:21:51 +0200

    Add Tombstone

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from "./question.js";
 export * from "./reject.js";
 export * from "./undo.js";
 export * from "./announce.js";
+export * from "./tombstone.js";
 
 export * from "./note/emoji.js";
 export * from "./note/mention.js";
